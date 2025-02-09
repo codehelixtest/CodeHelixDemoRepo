c99f1d9d5abfa2aec26ed14fedf3c120461190b2
codehelixtest <rogern@nealit.com>
2025-02-09 15:44:55 -0800

    Fix vulnerability in cmd5.php

--- Command Injection/cmd5.php
+++ Command Injection/cmd5.php
@@ -2,7 +2,21 @@
 <!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
 <?php  hint("not everything you need to inject is in a text input field ..."); ?>
 
+<?php include("../common/header.php"); ?>
 <form action="/CMD-5/index.php" method="GET">
+    <input type="text" name="domain" required>
+    <input type="hidden" name="server" value="whois.publicinterestregistry.net">
+</form>
+
+<pre>
+<?php
+if (preg_match('/^[-a-z0-9]+\.a[cdefgilmnoqrstuwxz]|b[abdefghijmnorstvwyz]|c[acdfghiklmnoruvxyz]|d[ejkmoz]|e[cegrstu]|f[ijkmor]|g[abdefghilmnpqrstuwy]|h[kmnrtu]|i[delmnoqrst]|j[emop]|k[eghimnprwyz]|l[abcikrstuvy]|m[acdeghklmnopqrstuvwxyz]|n[acefgilopruz]|om|p[aefghklmnrstwy]|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t[cdfghjklmnoprtvwz]|u[agksyz]|v[aceginu]|w[fs]|y[et]|z[amw]|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|aero|arpa|asia|coop|info|jobs|mobi|name|museum|travel|arpa|xn--[a-z0-9]+$/', strtolower($_GET["domain"]))) {
+    system("whois -h " . escapeshellarg($_GET["server"]) . " " . escapeshellarg($_GET["domain"]));
+} else {
+echo "malformed domain name";
+}
+?>
+</pre>
     <input type="text" name="domain">
     <input type="hidden" name="server" value="whois.publicinterestregistry.net">
 </form>
