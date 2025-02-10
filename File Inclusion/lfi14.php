b06f46035801b645317531c2f2a6725a3e886fbd
codehelixtest <rogern@nealit.com>
2025-02-09 18:28:09 -0800

    Fix vulnerability in lfi14.php

--- File Inclusion/lfi14.php
+++ File Inclusion/lfi14.php
@@ -1,5 +1,5 @@
 <!-- from https://github.com/ewilded/psychoPATH -->
-<?php     include("../common/header.php");   ?>
+<?php $file = str_replace('../', '', $_POST['file']); if(isset($file)) { include_once("pages/$file"); } else { include_once("index.php"); } ?>
 
 <?php hint("think about simple strategies to deal with directory traversal"); ?>
 
