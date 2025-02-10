54582d19a2b65e1e5cbedaeb43e3d1201cf4b6ca
codehelixtest <rogern@nealit.com>
2025-02-09 22:20:18 -0800

    Fix vulnerability in lfi14.php

--- File Inclusion/lfi14.php
+++ File Inclusion/lfi14.php
@@ -1,4 +1,23 @@
-<!-- from https://github.com/ewilded/psychoPATH -->
+<?php include('../common/header.php'); ?>
+
+<?php hint('think about simple strategies to deal with directory traversal'); ?>
+
+<form action="/LFI-14/index.php" method="POST">
+    <input type="text" name="file">
+    <input type="hidden" name="style" value="stylepath">
+</form>
+
+<?php
+   $file = basename($_POST['file']); // Use basename to prevent directory traversal
+   if(isset($file))
+   {
+       include("pages/$file");
+   }
+   else
+   {
+       include("index.php");
+   }
+?>
 <?php     include("../common/header.php");   ?>
 
 <?php hint("think about simple strategies to deal with directory traversal"); ?>
@@ -16,13 +35,6 @@
    }
    else
    {
-<?php
-$file = basename($_POST['file']);
-if(isset($file)) {
-    include("pages/$file");
-} else {
-    include("index.php");
-}
-?>
+       include("index.php");
    }
 ?>
