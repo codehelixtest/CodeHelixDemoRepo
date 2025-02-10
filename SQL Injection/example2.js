2b5aec48fd4457195da47fa7f6157b5cc6d22da1
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:20 -0800

    Fix vulnerability in example2.js

--- SQL Injection/example2.js
+++ SQL Injection/example2.js
@@ -24,8 +24,8 @@ new mysql.Database(
 	"INSERT INTO Customers (CustomerName, ContactName) VALUES ('Tom'," +
 	valTom + ")";
 	this.query(the_Query).execute(function(error, result)
-var the_Query = 'INSERT INTO Customers (CustomerName, ContactName) VALUES (?, ?)'; this.query(the_Query).execute([ 'Tom', valTom ], function(error, result) { ... });
-		if (error)
+	{
+var the_Query = 'INSERT INTO Customers (CustomerName, ContactName) VALUES (?, ?)'; this.query(the_Query, ['Tom', valTom]).execute(function(error, result) { ... });
 		{
 			console.log("Error: " + error);
 		}
