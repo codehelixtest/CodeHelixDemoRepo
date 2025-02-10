2389cec1e903e3bf908a8647c3d0fd3c25de012b
codehelixtest <rogern@nealit.com>
2025-02-09 18:28:52 -0800

    Fix vulnerability in sqli.php

--- SQL Injection/sqli.php
+++ SQL Injection/sqli.php
@@ -14,11 +14,11 @@ if (strlen($_GET['id']) < 1) {
   // an injection. The query result only returns a row count, making
   // it blind. It can be exploited based on whether the server
   // responds with "Yes!" or "No!"
-  $count = $db->querySingle('select count(*) from secrets where id = ' . $_GET['id']);
+$stmt = $db->prepare('SELECT count(*) FROM secrets WHERE id = :id'); $stmt->bindValue(':id', $_GET['id'], SQLITE3_INTEGER); $count = $stmt->execute()->fetchArray()[0];
 
   if ($count > 0) {
     echo 'Yes!';
-$count = $db->querySingle('select count(*) from secrets where id = ' . intval($_GET['id']));
+  } else {
     echo 'No!';
   }
 }
