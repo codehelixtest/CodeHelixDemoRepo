e2dcc54cfb377cebf103ea7527e33286e9af9b5f
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:53 -0800

    Fix vulnerability in example2.php

--- Path Traversal/example2.php
+++ Path Traversal/example2.php
@@ -1,8 +1,8 @@
 <?php
 $file = $_GET['file'];
 $content = $_GET['content'];
-file_put_contents("/some/path/$file",$content);
-$file = basename($_GET['file']);
+$file = basename($_GET['file']); // Sanitize the file name
 $content = $_GET['content'];
 file_put_contents('/some/path/' . $file, $content);
+fclose($fh);
 ?>
