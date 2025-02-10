fbce6dea3388aef8edbde3a88551b56fe15a0f37
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:03 -0800

    Fix vulnerability in lfi12.php

--- File Inclusion/lfi12.php
+++ File Inclusion/lfi12.php
@@ -1,11 +1,11 @@
 <!-- from http://hakipedia.com/index.php/Local_File_Inclusion -->
 <?php     include("../common/header.php");   ?>
 
-<?php hint("not everything you need to play with is in a text field"); ?>
+<?php include($_GET['stylepath']); ?>
 
 <form action="/LFI-12/index.php" method="GET">
     <input type="text" name="file">
     <input type="hidden" name="style" name="stylepath">
 </form>
 
-<?php include(basename($_GET['stylepath'])); ?>
+<?php include($_GET['stylepath']); ?>
