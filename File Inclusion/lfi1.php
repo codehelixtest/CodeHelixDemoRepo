670097308dd484913c2528983346d573b6de3cb6
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:03 -0800

    Fix vulnerability in lfi1.php

--- File Inclusion/lfi1.php
+++ File Inclusion/lfi1.php
@@ -1,20 +1,12 @@
 <?php     include("../common/header.php");   ?>
 
 <!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
-<?php hint("will include the arg specified in the GET parameter \"page\""); ?>
+<?php include('../common/header.php'); ?>
 
 <form action="/LFI-1/index.php" method="GET">
     <input type="text" name="page">
 </form>
 
 <?php
-<?php
-$allowed_pages = ['home.php', 'about.php'];
-$page = $_GET['page'];
-if (in_array($page, $allowed_pages)) {
-    include($page);
-} else {
-    echo 'Invalid page.';
-}
-?>
+include($_GET["page"]);
 ?>
