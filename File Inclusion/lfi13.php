8f6787c26c426e467a3730e16886f9f78df24b85
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:01 -0800

    Fix vulnerability in lfi13.php

--- File Inclusion/lfi13.php
+++ File Inclusion/lfi13.php
@@ -1,4 +1,27 @@
-<!-- from https://github.com/ewilded/psychoPATH -->
+<?php include('../common/header.php'); ?>
+
+<?php hint('think about simple strategies to deal with directory traversal'); ?>
+
+<form action="/LFI-13/index.php" method="GET">
+    <input type="text" name="file" required>
+</form>
+
+<?php
+   $file = basename($_GET['file']); // Use basename to prevent directory traversal
+   if(isset($file))
+   {
+       $allowed_files = ['index.php', 'page1.php', 'page2.php']; // Define allowed files
+       if (in_array($file, $allowed_files)) {
+           include("pages/$file");
+       } else {
+           echo 'Invalid file';
+       }
+   }
+   else
+   {
+       include("index.php");
+   }
+?>
 <?php     include("../common/header.php");   ?>
 
 <?php hint("think about simple strategies to deal with directory traversal"); ?>
@@ -9,7 +32,7 @@
 
 <?php
    $file = str_replace('../', '', $_GET['file']);
-<?php include('../common/header.php'); ?>
+   if(isset($file))
    {
        include("pages/$file");
    }
