9a34cfaf332c66e62a4d408a98183b6478f4b9ae
codehelixtest <rogern@nealit.com>
2025-02-09 22:20:21 -0800

    Fix vulnerability in phpexample.php

--- Path Traversal/phpexample.php
+++ Path Traversal/phpexample.php
@@ -1,3 +1,35 @@
+<?php
+
+$UploadDir = '/var/www/';
+
+if (!(isset($_GET['file'])))
+  die();
+
+$file = $_GET['file'];
+
+$path = $UploadDir . $file;
+
+if (!is_file($path))
+  die();
+
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Cache-Control: public');
+header('Content-Disposition: inline; filename="' . basename($path) . '";');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($path));
+
+$handle = fopen($path, 'rb');
+
+do {
+$data = fread($handle, 8192);
+if (strlen($data) == 0) {
+break;
+}
+echo($data);
+} while (true);
+
+fclose($handle);
+exit();
 
 <?php
 
@@ -32,35 +64,4 @@ echo($data);
 
 fclose($handle);
 exit();
-<?php
-
-$UploadDir = '/var/www/';
-
-if (!(isset($_GET['file'])))
-  die();
-
-$file = $_GET['file'];
-
-$path = $UploadDir . $file;
-
-if (!is_file($path))
-  die();
-
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-header('Cache-Control: public');
-header('Content-Disposition: inline; filename="' . basename($path) . '";');
-header('Content-Transfer-Encoding: binary');
-header('Content-Length: ' . filesize($path));
-
-$handle = fopen($path, 'rb');
-
-do {
-$data = fread($handle, 8192);
-if (strlen($data) == 0) {
-break;
-}
-echo($data);
-} while (true);
-
-fclose($handle);
-exit();
+?>
