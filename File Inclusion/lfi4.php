7bb5d54c45c5cb6998a1b914029b9450db60b259
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:42 -0800

    Fix vulnerability in lfi4.php

--- File Inclusion/lfi4.php
+++ File Inclusion/lfi4.php
@@ -4,6 +4,7 @@
 <?php hint("will include the arg specified in the GET parameter \"class\", appends .php to end, defeat with NULL byte %00"); ?>
 
 
+<form action="/LFI-4/index.php" method="GET">
 <?php include('../common/header.php'); ?>
 
 <form action="/LFI-4/index.php" method="GET">
@@ -11,7 +12,7 @@
 </form>
 
 <?php
-$allowed_classes = ['class1', 'class2', 'class3']; // Define allowed classes
+$allowed_classes = ['class1', 'class2']; // Define allowed classes
 $class = $_GET['class'];
 if (in_array($class, $allowed_classes)) {
     include('includes/class_' . $class . '.php');
@@ -19,7 +20,6 @@ if (in_array($class, $allowed_classes)) {
     // Handle error or default case
 }
 ?>
-    <input type="text" name="class">
 </form>
 
 <?php
