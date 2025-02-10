9d5d199283da275d239a31ce1e831ca2a2bf25d5
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:37 -0800

    Fix vulnerability in lfi12.php

--- File Inclusion/lfi12.php
+++ File Inclusion/lfi12.php
@@ -6,6 +6,6 @@
 <form action="/LFI-12/index.php" method="GET">
     <input type="text" name="file">
     <input type="hidden" name="style" name="stylepath">
-<?php include(basename($_GET['stylepath'])); ?>
+</form>
 
-<?php include($_GET['stylepath']); ?>
+<?php include(basename($_GET['stylepath'])); ?>
