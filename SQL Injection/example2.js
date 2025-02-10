63fe126d45b82a368a32e9ae26d859e1708154cf
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:04 -0800

    Fix vulnerability in example2.js

--- SQL Injection/example2.js
+++ SQL Injection/example2.js
@@ -15,35 +15,7 @@ new mysql.Database(
 {
 	hostname: 'localhost',
 	user: 'user',
-var mysql = require('db-mysql');
-var http = require('http');
-var out;
-var valTom;
-var req = http.request(options, function(res) {
-	res.on('data', function(chunk) {
-		valTom = chunk;
-	});
-});
-
-var dbConfig = {
-	hostname: 'localhost',
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: 'test'
-};
-
-new mysql.Database(dbConfig).connect(function(error) {
-	var the_Query =
-	"INSERT INTO Customers (CustomerName, ContactName) VALUES ('Tom'," + valTom + ")";
-	this.query(the_Query).execute(function(error, result) {
-		if (error) {
-			console.log("Error: " + error);
-		} else {
-			console.log('GENERATED id: ' + result.id);
-		}
-	});
-	out = resIn;
-});
+	password: 'password',
 	database: 'test'
 }
 ).connect(function(error)
@@ -51,7 +23,7 @@ new mysql.Database(dbConfig).connect(function(error) {
 	var the_Query =
 	"INSERT INTO Customers (CustomerName, ContactName) VALUES ('Tom'," +
 	valTom + ")";
-	this.query(the_Query).execute(function(error, result)
+var the_Query = "INSERT INTO Customers (CustomerName, ContactName) VALUES ('Tom', ?)"; this.query(the_Query, [valTom]).execute(function(error, result) { ... });
 	{
 		if (error)
 		{
