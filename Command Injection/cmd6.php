aac7269df0afd1fd72c56d68c4d582d21684e6e9
codehelixtest <rogern@nealit.com>
2025-02-09 18:29:19 -0800

    Fix vulnerability in cmd6.php

--- Command Injection/cmd6.php
+++ Command Injection/cmd6.php
@@ -9,8 +9,8 @@
 
 <pre>
 <?php
-if (preg_match('/^[-a-z0-9]+\.a[cdefgilmnoqrstuwxz]|b[abdefghijmnorstvwyz]|c[acdfghiklmnoruvxyz]|d[ejkmoz]|e[cegrstu]|f[ijkmor]|g[abdefghilmnpqrstuwy]|h[kmnrtu]|i[delmnoqrst]|j[emop]|k[eghimnprwyz]|l[abcikrstuvy]|m[acdeghklmnopqrstuvwxyz]|n[acefgilopruz]|om|p[aefghklmnrstwy]|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t[cdfghjklmnoprtvwz]|u[agksyz]|v[aceginu]|w[fs]|y[et]|z[amw]|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|aero|arpa|asia|coop|info|jobs|mobi|name|museum|travel|arpa|xn--[a-z0-9]+$/', strtolower($_POST["domain"])))
-$domain = escapeshellarg(strtolower($_POST['domain'])); system('whois -h ' . $_POST['server'] . ' ' . $domain);
+<?php if (preg_match('/^[-a-z0-9]+\.a[cdefgilmnoqrstuwxz]|b[abdefghijmnorstvwyz]|c[acdfghiklmnoruvxyz]|d[ejkmoz]|e[cegrstu]|f[ijkmor]|g[abdefghilmnpqrstuwy]|h[kmnrtu]|i[delmnoqrst]|j[emop]|k[eghimnprwyz]|l[abcikrstuvy]|m[acdeghklmnopqrstuvwxyz]|n[acefgilopruz]|om|p[aefghklmnrstwy]|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t[cdfghjklmnoprtvwz]|u[agksyz]|v[aceginu]|w[fs]|y[et]|z[amw]|z[amw]|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|aero|arpa|asia|coop|info|jobs|mobi|name|museum|travel|arpa|xn--[a-z0-9]+$/', strtolower($_POST['domain']))) { $domain = escapeshellarg($_POST['domain']); $server = escapeshellarg($_POST['server']); system('whois -h ' . $server . ' ' . $domain); } else { echo 'malformed domain name'; } ?>
+        { system("whois -h " . $_POST["server"] . " " . $_POST["domain"]); } 
     else 
         {echo "malformed domain name";}
     
