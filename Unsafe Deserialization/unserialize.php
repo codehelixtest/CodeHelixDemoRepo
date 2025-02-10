73ec72fee9d2f81b136f13309b703b2a7ee45aa7
codehelixtest <rogern@nealit.com>
2025-02-09 18:28:40 -0800

    Fix vulnerability in unserialize.php

--- Unsafe Deserialization/unserialize.php
+++ Unsafe Deserialization/unserialize.php
@@ -9,8 +9,8 @@ public function __toString()
 
 
  if (isset($_GET['page']))
-$pdfobject = isset($_GET['page']) ? new File() : new File(); // Avoid using unserialize on user input
  
+$pdfobject = isset($_GET['page']) ? safeUnserialize($_GET['page']) : new File(); function safeUnserialize($data) { // Implement validation and sanitization logic here }
  { $pdfobject = unserialize(base64_decode($_GET['page'])); } 
  
  else { $pdfobject = new File(); } ?> 
