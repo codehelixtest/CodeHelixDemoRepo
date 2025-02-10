096bd13e951a2193fe98a98291fada130a36ab02
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:24 -0800

    Fix vulnerability in lfi8.php

--- File Inclusion/lfi8.php
+++ File Inclusion/lfi8.php
@@ -1,22 +1,15 @@
 <?php     include("../common/header.php");   ?>
 
-<?php include("../common/header.php"); ?>
-
-<form action="/LFI-8/index.php" method="POST">
-    <input type="text" name="file" required>
-</form>
-
-<?php
-if (isset($_POST['file']) && substr($_POST['file'], -4) === '.php') {
-    echo 'You are not allowed to see source files!' . "\n";
-} else {
-    $file = basename($_POST['file']); // Sanitize input
-    echo file_get_contents($file);
-}
-?>
+<!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
 <?php hint("will include the arg specified in the POST parameter \"file\", looks for .php at end - bypass by apending /. (slash plus dot)"); ?>
 
-<form action="/LFI-8/index.php" method="POST">
+<?php
+if (isset($_POST['file']) && preg_match('/^[a-zA-Z0-9_\-]+\.php$/', $_POST['file'])) {
+    echo file_get_contents($_POST['file']);
+} else {
+    echo 'You are not allowed to see source files!' . "\n";
+}
+?>
     <input type="text" name="file">
 </form>
 
