c078e8f4db37506cfc4f20fef9ba27937535ef4f
codehelixtest <rogern@nealit.com>
2025-02-09 22:20:37 -0800

    Fix vulnerability in phpexample.php

--- Path Traversal/phpexample.php
+++ Path Traversal/phpexample.php
@@ -1,35 +1,3 @@
-<?php
-
-$UploadDir = '/var/www/';
-
-if (!(isset($_GET['file'])))
-  die();
-
-$file = $_GET['file'];
-
-$path = $UploadDir . $file;
-
-if (!is_file($path))
-  die();
-
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-header('Cache-Control: public');
-header('Content-Disposition: inline; filename="' . basename($path) . '";');
-header('Content-Transfer-Encoding: binary');
-header('Content-Length: ' . filesize($path));
-
-$handle = fopen($path, 'rb');
-
-do {
-$data = fread($handle, 8192);
-if (strlen($data) == 0) {
-break;
-}
-echo($data);
-} while (true);
-
-fclose($handle);
-exit();
 
 <?php
 
@@ -41,6 +9,7 @@ if (!(isset($_GET['file'])))
 
 $file = $_GET['file'];
 
+$file = basename($_GET['file']); // Use basename to prevent path traversal
 $path = $UploadDir . $file;
 
 if (!is_file($path))
