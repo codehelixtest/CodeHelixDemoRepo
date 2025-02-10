27cf59ef2582a9625482a2b2e21f5fb51e927597
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:36 -0800

    Fix vulnerability in unserialize.php

--- Unsafe Deserialization/unserialize.php
+++ Unsafe Deserialization/unserialize.php
@@ -1,10 +1,10 @@
 class ViewFile { public $filename = ''; 
 
 
-if (isset($_GET['page'])) { $pdfobject = json_decode(base64_decode($_GET['page']), true); // Use a safer alternative to unserialize } else { $pdfobject = new File(); }
+public function __toString()
 
 
- { include $this->filename; return ""; } }
+if (isset($_GET['page'])) { $pdfobject = json_decode(base64_decode($_GET['page']), true); } else { $pdfobject = new File(); } // Use a safer serialization format
 
 
 
