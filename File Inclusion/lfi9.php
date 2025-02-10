16f5efb76b3004b9b5af2f99b96d6d8b85b32347
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:17 -0800

    Fix vulnerability in lfi9.php

--- File Inclusion/lfi9.php
+++ File Inclusion/lfi9.php
@@ -2,13 +2,13 @@
 
 <!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
 
-include('includes/class_' . basename($_POST['class']) . '.php');
+<?php hint("will include the arg specified in the POST parameter \"class\", appends .php to end, defeat with NULL byte %00"); ?>
 
 <form action="/LFI-9/index.php" method="POST">
     <input type="text" name="class">
 </form>
 
 <?php
-include('includes/class_'.addslashes($_POST['class']).'.php');
+<?php include('includes/class_'.basename($_POST['class']).'.php'); ?>
 ?>
 
