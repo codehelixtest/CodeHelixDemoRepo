09a0da44dc299bb58249335f606660bfa9ce273c
codehelixtest <rogern@nealit.com>
2025-02-09 18:29:27 -0800

    Fix vulnerability in example2.php

--- Insecure File Uploads/example2.php
+++ Insecure File Uploads/example2.php
@@ -1,4 +1,4 @@
-<?php
+
 // Is it an image?
 if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) &&
 	( $uploaded_size < 100000 ) ) {
@@ -17,7 +17,6 @@ else {
 	// Invalid file
 	$html .= '<pre>Your image was not uploaded. We can only accept JPEG or PNG images.</pre>';
 }
-	// Is it an image?
 	if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) &&
 		( $uploaded_size < 100000 ) ) {
 
