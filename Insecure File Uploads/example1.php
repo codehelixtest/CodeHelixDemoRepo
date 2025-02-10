dd5ecaf902b43dba518216d615aa0b0b0b41c43e
codehelixtest <rogern@nealit.com>
2025-02-09 18:28:44 -0800

    Fix vulnerability in example1.php

--- Insecure File Uploads/example1.php
+++ Insecure File Uploads/example1.php
@@ -2,7 +2,7 @@
 
 if( isset( $_POST[ 'Upload' ] ) ) {
 	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
+$target_path .= preg_replace('/[^a-zA-Z0-9._-]/', '', basename($_FILES['uploaded']['name']));
 	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
 
 	// Can we move the file to the upload folder?
@@ -16,21 +16,4 @@ if( isset( $_POST[ 'Upload' ] ) ) {
 	}
 }
 
-<?php
-
-if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
-	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
-	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
-
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
-	}
-	else {
-		// Yes!
-		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
-	}
-}
-
+?>
