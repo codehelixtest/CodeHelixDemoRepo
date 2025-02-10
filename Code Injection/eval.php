70562632bfe99d573ecf11ec81175576e999a738
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:54 -0800

    Fix vulnerability in eval.php

--- Code Injection/eval.php
+++ Code Injection/eval.php
@@ -3,8 +3,8 @@
 require_once('../_helpers/strip.php');
 
 // first, get a variable name based on the user input
-$variable = isset($_GET['variable']) ? $_GET['variable'] : 'empty'; // Avoid using eval and sanitize input
-$empty = 'No variable given';
+$variable = strlen($_GET['variable']) > 0 ? $_GET['variable'] : 'empty';
+$variable = isset($_GET['variable']) ? $_GET['variable'] : 'empty'; // Ensure proper validation and sanitization before using the variable
 
 // pass the variable name into an eval block, making it
 // vulnerable to Remote Code Execution (rce). This RCE
