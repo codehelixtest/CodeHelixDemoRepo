ab71c05f4a2b510771862dce087e6aa5131b54b8
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:36 -0800

    Fix vulnerability in cmd6.php

--- Command Injection/cmd6.php
+++ Command Injection/cmd6.php
@@ -3,19 +3,25 @@
 <?php  hint("not everything you need to inject is in a text input field ..."); ?>
 
 <form action="/CMD-6/index.php" method="POST">
+    <input type="text" name="domain">
 <?php include("../common/header.php"); ?>
 <form action="/CMD-6/index.php" method="POST">
     <input type="text" name="domain">
     <input type="hidden" name="server" value="whois.publicinterestregistry.net">
 </form>
+
+<pre>
 <?php
 if (preg_match('/^[-a-z0-9]+\.a[cdefgilmnoqrstuwxz]|b[abdefghijmnorstvwyz]|c[acdfghiklmnoruvxyz]|d[ejkmoz]|e[cegrstu]|f[ijkmor]|g[abdefghilmnpqrstuwy]|h[kmnrtu]|i[delmnoqrst]|j[emop]|k[eghimnprwyz]|l[abcikrstuvy]|m[acdeghklmnopqrstuvwxyz]|n[acefgilopruz]|om|p[aefghklmnrstwy]|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t[cdfghjklmnoprtvwz]|u[agksyz]|v[aceginu]|w[fs]|y[et]|z[amw]|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|aero|arpa|asia|coop|info|jobs|mobi|name|museum|travel|arpa|xn--[a-z0-9]+$/', strtolower($_POST["domain"]))) {
-    system("whois -h " . $_POST["server"] . " " . $_POST["domain"]);
+    // Use a safer method to handle the domain lookup
+    $domain = escapeshellarg($_POST["domain"]);
+    $server = escapeshellarg($_POST["server"]);
+    system("whois -h " . $server . " " . $domain);
 } else {
-echo "malformed domain name";
+    echo "malformed domain name";
 }
 ?>
-    <input type="hidden" name="server" value="whois.publicinterestregistry.net">
+</pre>
 </form>
 
 <pre>
