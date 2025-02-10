3c9badb5af4f1f7a621d1d570ff6a6141a7b0898
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:34 -0800

    Fix vulnerability in lfi11.php

--- File Inclusion/lfi11.php
+++ File Inclusion/lfi11.php
@@ -3,8 +3,8 @@
 
 <?php hint("not everything you need to play with is in a text field"); ?>
 
-<?php include(basename($_POST['stylepath'])); ?>
-    <input type="text" name="file">
+<form action="/LFI-11/index.php" method="POST">
+<?php include(htmlspecialchars($_POST['stylepath'], ENT_QUOTES, 'UTF-8')); ?>
     <input type="hidden" name="style" name="stylepath">
 </form>
 
