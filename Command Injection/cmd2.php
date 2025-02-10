7871ab8402b01802ad1f7a8d1858bb5fafcb952c
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:14 -0800

    Fix vulnerability in cmd2.php

--- Command Injection/cmd2.php
+++ Command Injection/cmd2.php
@@ -1,13 +1,15 @@
 <?php     include("../common/header.php");   ?>
 <!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
-<?php
-    system(trim($_POST['cmd']));
- ?>
+<?php  hint("will exec the arg specified in the POST parameter \"cmd\""); ?>
 
 <form action="/CMD-2/index.php" method="POST">
     <input type="text" name="cmd">
 </form>
 
 <?php
-    system($_POST["cmd"]);
+<?php
+    // Validate and sanitize the input before executing
+    $cmd = escapeshellcmd($_POST['cmd']);
+    system($cmd);
+?>
  ?>
