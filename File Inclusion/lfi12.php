d37e53db6f251a857f57b1cac652cf0abfb8f0c4
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:44 -0800

    Fix vulnerability in lfi12.php

--- File Inclusion/lfi12.php
+++ File Inclusion/lfi12.php
@@ -4,8 +4,8 @@
 <?php hint("not everything you need to play with is in a text field"); ?>
 
 <form action="/LFI-12/index.php" method="GET">
+    <input type="text" name="file">
 <?php include(htmlspecialchars($_GET['stylepath'])); ?>
-    <input type="hidden" name="style" name="stylepath">
 </form>
 
 <?php include($_GET['stylepath']); ?>
