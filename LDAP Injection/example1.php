1aef250d7ed30421944d7e4f73eaa7b32721a5ba
codehelixtest <rogern@nealit.com>
2025-02-09 18:28:59 -0800

    Fix vulnerability in example1.php

--- LDAP Injection/example1.php
+++ LDAP Injection/example1.php
@@ -1,8 +1,8 @@
 <?php
-$dn = $_GET['host'];
+$dn = filter_input(INPUT_GET, 'host', FILTER_SANITIZE_STRING);
 $filter="(|(sn=$person*)(givenname=$person*))";
 $justthese = array("ou", "sn", "givenname", "mail");
 $sr=ldap_search($ds, $dn, $dn, $justthese);
 $info = ldap_get_entries($ds, $sr);
-$dn = filter_input(INPUT_GET, 'host', FILTER_SANITIZE_STRING); $filter = "(|(sn=$person*)(givenname=$person*))"; // Ensure $person is also sanitized before use.
+echo $info["count"]." entries returned
 ";?>
