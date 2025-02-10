174611d4a9d4f484be037429fe5c02483fa4769a
codehelixtest <rogern@nealit.com>
2025-02-09 18:32:11 -0800

    Fix vulnerability in chall2.php

--- PHP Object Injection/chall2.php
+++ PHP Object Injection/chall2.php
@@ -8,8 +8,8 @@ class Example2
    }
 
    function __wakeup()
+   {
 function __wakeup() { if (isset($this->hook)) { throw new Exception('Invalid hook'); } }
-      if (isset($this->hook)) eval($this->hook);
    }
 }
 
