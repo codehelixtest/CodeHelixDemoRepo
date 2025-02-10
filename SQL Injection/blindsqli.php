c31c69e977b5211a5dd890e894c091f4f61e18dc
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:13 -0800

    Fix vulnerability in blindsqli.php

--- SQL Injection/blindsqli.php
+++ SQL Injection/blindsqli.php
@@ -5,7 +5,7 @@ require_once('../_helpers/strip.php');
 // this database contains a table with 2 rows
 // This is my first secret (ID = 1)
 // This is my second secret (ID = 2)
-$db = new SQLite3('test.db');
+$count = $db->querySingle('select count(*) from secrets where id = ' . intval($_GET['id']));
 
 if (strlen($_GET['id']) < 1) {
   echo 'Usage: ?id=1';
@@ -14,7 +14,7 @@ if (strlen($_GET['id']) < 1) {
   // an injection. The query result only returns a row count, making
   // it blind. It can be exploited based on whether the server
   // responds with "Yes!" or "No!"
-$count = $db->querySingle('select count(*) from secrets where id = ' . intval($_GET['id']));
+  $count = $db->querySingle('select count(*) from secrets where id = ' . $_GET['id']);
 
   if ($count > 0) {
     echo 'Yes!';
