3467999689185977b26cc239b50dabd2061ec889
codehelixtest <rogern@nealit.com>
2025-02-09 18:32:52 -0800

    Fix vulnerability in LogFile.java

--- Unsafe Deserialization/java/LogFile.java
+++ Unsafe Deserialization/java/LogFile.java
@@ -2,7 +2,11 @@
 
 class LogFile implements Serializable
 {
-   public String filename;
+class LogFile implements Serializable {
+   private String filename;
+   private String filecontent;
+   // ... rest of the code ...
+}
    public String filecontent;
 
   // Function called during deserialization
@@ -32,23 +36,7 @@ class LogFile implements Serializable
      }
      catch (Exception e)
      {
-private void readObject(ObjectInputStream in) {
-    System.out.println("readObject from LogFile");
-    try {
-        in.defaultReadObject();
-        // Use a proper logging framework instead of System.out.println
-        Logger logger = Logger.getLogger(LogFile.class.getName());
-        logger.info("File name: " + filename);
-        // Avoid logging file content
-        FileWriter file = new FileWriter(filename);
-        BufferedWriter out = new BufferedWriter(file);
-        out.write(filecontent);
-        out.close();
-        file.close();
-    } catch (Exception e) {
-        System.out.println("Exception: " + e.toString());
-    }
-}
+         System.out.println("Exception: " + e.toString());
      }
    }
 }
