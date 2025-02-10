c879c6062afae6b060fb77f450b7f334a63bc3c5
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:10 -0800

    Fix vulnerability in lfi6.php

--- File Inclusion/lfi6.php
+++ File Inclusion/lfi6.php
@@ -1,8 +1,22 @@
 <?php     include("../common/header.php");   ?>
 
-<!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
-
 <?php include('../common/header.php'); ?>
+
+<form action="/LFI-6/index.php" method="POST">
+    <input type="text" name="page">
+</form>
+
+<?php
+$page = basename($_POST['page']); // Sanitize input
+$allowed_pages = ['page1.php', 'page2.php']; // Define allowed pages
+if (in_array($page, $allowed_pages)) {
+    include($page);
+} else {
+    echo 'Invalid page';
+}
+?>
+
+<?php hint("will include the arg specified in the POST parameter \"page\"");  ?>
 
 
 <form action="/LFI-6/index.php" method="POST">
