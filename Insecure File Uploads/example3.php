9875762a4a5ff72a827f6442b2a5b5b98c139526
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:28 -0800

    Fix vulnerability in example3.php

--- Insecure File Uploads/example3.php
+++ Insecure File Uploads/example3.php
@@ -1,11 +1,9 @@
 	$uploaded_name = $_FILES[ 'uploaded' ][ 'name' ];
 	$uploaded_ext  = substr( $uploaded_name, strrpos( $uploaded_name, '.' ) + 1);
 	$uploaded_size = $_FILES[ 'uploaded' ][ 'size' ];
-// Use a templating engine or separate HTML from PHP logic to avoid inline HTML.
-$html .= htmlspecialchars('<pre>Your image was not uploaded.</pre>');
-$html .= htmlspecialchars('<pre>{$target_path} successfully uploaded!</pre>');
+	$uploaded_tmp  = $_FILES[ 'uploaded' ][ 'tmp_name' ];
 
-	// Is it an image?
+$html .= htmlspecialchars('<pre>Your image was not uploaded.</pre>');
 	if( ( strtolower( $uploaded_ext ) == "jpg" || strtolower( $uploaded_ext ) == "jpeg" || strtolower( $uploaded_ext ) == "png" ) &&
 		( $uploaded_size < 100000 ) &&
 		getimagesize( $uploaded_tmp ) ) {
