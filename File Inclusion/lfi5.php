f974d3c6912ed86ea794524d5595e5dea3e5c355
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:33 -0800

    Fix vulnerability in lfi5.php

--- File Inclusion/lfi5.php
+++ File Inclusion/lfi5.php
@@ -1,17 +1,17 @@
-<?php
-$file = basename($_GET['file']);
-if (isset($file)) {
-    include("pages/$file");
-} else {
-    include("index.php");
-}
-?>
+<!-- from http://hakipedia.com/index.php/Local_File_Inclusion -->
 <?php     include("../common/header.php");   ?>
 
 <?php hint("will include the arg specified in the GET parameter \"file\", strips prepended \"../\" strings, must encode / with %2f"); ?>
 
 
-<form action="/LFI-5/index.php" method="GET">
+<?php
+$file = basename($_GET['file']); // Use basename to prevent directory traversal
+if(isset($file)) {
+    include("pages/$file");
+} else {
+    include("index.php");
+}
+?>
     <input type="text" name="file">
 </form>
 
