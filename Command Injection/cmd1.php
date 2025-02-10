7d240f8f1cd0cfa1da816acc3653c6e447d33dea
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:48 -0800

    Fix vulnerability in cmd1.php

--- Command Injection/cmd1.php
+++ Command Injection/cmd1.php
@@ -1,23 +1,28 @@
 <?php     include("../common/header.php");   ?>
 
-<?php include("../common/header.php"); ?>
+<!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
+<?php
+hint("will exec the arg specified in the GET parameter \"cmd\"");
+?>
+
+<?php
+include("../common/header.php");
+?>
 
 <form action="/CMD-1/index.php" method="GET">
     <input type="text" name="cmd">
 </form>
 
 <?php
-    // Avoid using system with unsanitized input
-    if (isset($_GET['cmd'])) {
-        $cmd = escapeshellcmd($_GET['cmd']); // Sanitize input
-        system($cmd);
-    }
+// Avoid using system() with user input
+// system escapes the input, consider using a whitelist or other validation
+$allowedCommands = ['ls', 'whoami']; // Example of allowed commands
+if (in_array($_GET['cmd'], $allowedCommands)) {
+    system($_GET['cmd']);
+} else {
+    echo 'Command not allowed';
+}
 ?>
-<?php
-hint("will exec the arg specified in the GET parameter \"cmd\"");
-?>
-
-<form action="/CMD-1/index.php" method="GET">
     <input type="text" name="cmd">
 </form>
 
