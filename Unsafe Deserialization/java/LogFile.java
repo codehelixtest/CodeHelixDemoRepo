d88397fa1d0b482d214b3e0943a5dd5a5f09470c
codehelixtest <rogern@nealit.com>
2025-02-09 18:32:55 -0800

    Fix vulnerability in LogFile.java

--- Unsafe Deserialization/java/LogFile.java
+++ Unsafe Deserialization/java/LogFile.java
@@ -1,16 +1,9 @@
 // Vulnerable class
 
-class LogFile implements Serializable
+public LogFile() {}
 {
    public String filename;
-class LogFile implements Serializable {
-   private String filename;
-   private String filecontent;
-   // Function called during deserialization
-   private void readObject(ObjectInputStream in) {
-      // ... existing code ...
-   }
-}
+   public String filecontent;
 
   // Function called during deserialization
 
