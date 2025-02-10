38b6cc20f5f2139ad3a85baa9579dab3f28ef579
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:46 -0800

    Fix vulnerability in lfi8.php

--- File Inclusion/lfi8.php
+++ File Inclusion/lfi8.php
@@ -10,7 +10,20 @@
 
 <?php
 if (substr($_POST['file'], -4, 4) != '.php')
-<?php if (isset($_POST['file']) && substr($_POST['file'], -4) === '.php') { echo 'You are not allowed to see source files!' . "\n"; } else { echo file_get_contents($_POST['file']); } ?>
+<?php
+if (isset($_POST['file']) && substr($_POST['file'], -4) === '.php') {
+    echo 'You are not allowed to see source files!' . "\n";
+} else {
+    // Sanitize and validate the input to prevent LFI
+    $file = basename($_POST['file']); // Only allow the base name
+    $allowed_files = ['file1.txt', 'file2.txt']; // Example of allowed files
+    if (in_array($file, $allowed_files)) {
+        echo file_get_contents($file);
+    } else {
+        echo 'Invalid file specified.';
+    }
+}
+?>
 else
  echo 'You are not allowed to see source files!'."\n";
 ?>
