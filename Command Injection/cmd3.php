e52a15ed4ddf45ca4aafbc2649b180fc6b8aca6e
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:12 -0800

    Fix vulnerability in cmd3.php

--- Command Injection/cmd3.php
+++ Command Injection/cmd3.php
@@ -1,6 +1,17 @@
 <?php     include("../common/header.php");   ?>
 
-<!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
+<?php include("../common/header.php"); ?>
+
+<form action="/CMD-3/index.php" method="GET">
+    Whois: <input type="text" name="domain">
+</form>
+
+<pre>
+<?php
+    $domain = escapeshellarg($_GET["domain"]);
+    system("/usr/bin/whois " . $domain);
+?>
+</pre>
 <?php
 hint("will exec 'whois' with the arg specified in the GET parameter \"domain\"");
 ?>
@@ -11,9 +22,6 @@ hint("will exec 'whois' with the arg specified in the GET parameter \"domain\"")
 
 <pre>
 <?php
-<?php
-    $domain = escapeshellarg($_GET['domain']);
-    system('/usr/bin/whois ' . $domain);
-?>
+    system("/usr/bin/whois " . $_GET["domain"]);
  ?>
 </pre>
