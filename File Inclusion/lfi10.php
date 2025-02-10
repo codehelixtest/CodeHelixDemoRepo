900299bb05f88ac85814a01480b7c86b0f3303ca
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:31 -0800

    Fix vulnerability in lfi10.php

--- File Inclusion/lfi10.php
+++ File Inclusion/lfi10.php
@@ -3,18 +3,15 @@
 
 <?php hint("will include the arg specified in the POST parameter \"file\", strips prepended \"../\" strings, must encode / with %2f"); ?>
 
+<form action="/LFI-10/index.php" method="POST">
 <?php
-   $file = basename($_POST['file']); // Use basename to prevent directory traversal
-   if(isset($file))
-   {
-       include("pages/$file");
-   }
-   else
-   {
-       include("index.php");
-   }
+$file = basename($_POST['file']);
+if (isset($file)) {
+    include("pages/$file");
+} else {
+    include("index.php");
+}
 ?>
-    <input type="text" name="file">
 </form>
 
 <?php
