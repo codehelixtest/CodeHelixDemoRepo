950cfcbd33d86c7b2b41ebb5d13f45a10e535940
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:49 -0800

    Fix vulnerability in cmd5.php

--- Command Injection/cmd5.php
+++ Command Injection/cmd5.php
@@ -4,22 +4,20 @@
 
 <form action="/CMD-5/index.php" method="GET">
     <input type="text" name="domain">
+    <input type="hidden" name="server" value="whois.publicinterestregistry.net">
 <?php include("../common/header.php"); ?>
 <form action="/CMD-5/index.php" method="GET">
     <input type="text" name="domain" required>
     <input type="hidden" name="server" value="whois.publicinterestregistry.net">
     <input type="submit" value="Submit">
 </form>
-<pre>
 <?php
-if (preg_match('/^[-a-z0-9]+\.a[cdefgilmnoqrstuwxz]|b[abdefghijmnorstvwyz]|c[acdfghiklmnoruvxyz]|d[ejkmoz]|e[cegrstu]|f[ijkmor]|g[abdefghilmnpqrstuwy]|h[kmnrtu]|i[delmnoqrst]|j[emop]|k[eghimnprwyz]|l[abcikrstuvy]|m[acdeghklmnopqrstuvwxyz]|n[acefgilopruz]|om|p[aefghklmnrstwy]|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t[cdfghjklmnoprtvwz]|u[agksyz]|v[aceginu]|w[fs]|y[et]|z[amw]|z[amw]|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|aero|arpa|asia|coop|info|jobs|mobi|name|museum|travel|arpa|xn--[a-z0-9]+$/', strtolower($_GET["domain"]))) {
+if (preg_match('/^[-a-z0-9]+\.a[cdefgilmnoqrstuwxz]|b[abdefghijmnorstvwyz]|c[acdfghiklmnoruvxyz]|d[ejkmoz]|e[cegrstu]|f[ijkmor]|g[abdefghilmnpqrstuwy]|h[kmnrtu]|i[delmnoqrst]|j[emop]|k[eghimnprwyz]|l[abcikrstuvy]|m[acdeghklmnopqrstuvwxyz]|n[acefgilopruz]|om|p[aefghklmnrstwy]|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t[cdfghjklmnoprtvwz]|u[agksyz]|v[aceginu]|w[fs]|y[et]|z[amw]|biz|cat|com|edu|gov|int|mil|net|org|pro|tel|aero|arpa|asia|coop|info|jobs|mobi|name|museum|travel|arpa|xn--[a-z0-9]+$/', strtolower($_GET["domain"]))) {
     system("whois -h " . $_GET["server"] . " " . $_GET["domain"]);
 } else {
     echo "malformed domain name";
 }
 ?>
-</pre>
-</form>
 
 <pre>
 <?php
