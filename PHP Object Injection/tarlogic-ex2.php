df039c1f5dbecf6c2212b2f47fab8a68053ca79a
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:00 -0800

    Fix vulnerability in tarlogic-ex2.php

--- PHP Object Injection/tarlogic-ex2.php
+++ PHP Object Injection/tarlogic-ex2.php
@@ -16,42 +16,10 @@ class GiveFlag extends File {
   public function innocent() {
     $stuff = fopen("flag.txt", "r");
     fseek($stuff, $this->offset);
-    print fread($stuff, filesize("flag.txt"));
+$data = unserialize($argv[1]); if ($data instanceof ExpectedClass) { /* proceed with safe operations */ } else { /* handle error */ }
   }
 }
-<?php
-// Exercise - 2
-// Author: @TheXC3LL
-// Website: Tarlogic.com
-// Modificado de: https://syssec.rub.de/media/emma/veroeffentlichungen/2014/09/10/POPChainGeneration-CCS14.pdf
-
-class File {
-  public function flag() {
-    $this->innocent();
-  }
-  public function innocent() {
-    echo "Aquí no pasa nada :D\n";
-  }
-}
-
-class GiveFlag extends File {
-  public $offset = 23;
-  public function innocent() {
-    $stuff = fopen("flag.txt", "r");
-    fseek($stuff, $this->offset);
-    print fread($stuff, filesize("flag.txt"));
-  }
-}
-
-// Separate the entry class into its own file
-// class entry {
-//   public function __destruct(){
-//     $this->awesome->flag();
-//   }
-// }
-
-unserialize($argv[1]);
-?>
+class entry {
   public function __destruct(){
     $this->awesome->flag();
   }
