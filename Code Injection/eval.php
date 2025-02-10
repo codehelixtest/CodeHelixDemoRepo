f5218d7e4d9ea7a17d73b5fb01842981f2b47d38
codehelixtest <rogern@nealit.com>
2025-02-09 18:30:11 -0800

    Fix vulnerability in eval.php

--- Code Injection/eval.php
+++ Code Injection/eval.php
@@ -3,12 +3,7 @@
 require_once('../_helpers/strip.php');
 
 // first, get a variable name based on the user input
-$variable = isset($_GET['variable']) ? $_GET['variable'] : 'empty';
-if (preg_match('/^[a-zA-Z0-9_]+$/', $variable)) {
-    echo $$variable;
-} else {
-    echo 'Invalid variable';
-}
+$variable = preg_replace('/[^a-zA-Z0-9_]/', '', $_GET['variable']); eval('echo $' . $variable . ';');
 $empty = 'No variable given';
 
 // pass the variable name into an eval block, making it
