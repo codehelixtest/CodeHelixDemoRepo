6b9dc4de1670565e530233bddb24e3959ea7d7eb
codehelixtest <rogern@nealit.com>
2025-02-09 18:32:58 -0800

    Fix vulnerability in lfi3.php

--- File Inclusion/lfi3.php
+++ File Inclusion/lfi3.php
@@ -6,25 +6,12 @@
 
 <form action="/LFI-3/index.php" method="GET">
     <input type="text" name="file">
-<?php include('../common/header.php'); ?>
-
-<form action="/LFI-3/index.php" method="GET">
-    <input type="text" name="file">
 </form>
-
-<?php
-$allowed_files = ['allowed_file1.php', 'allowed_file2.php']; // Define allowed files
-if (in_array($_GET['file'], $allowed_files)) {
-    echo file_get_contents($_GET['file']);
-} else {
-    echo 'You are not allowed to see source files!' . "\n";
-}
-?>
 
 
 <?php
 if (substr($_GET['file'], -4, 4) != '.php')
- echo file_get_contents($_GET['file']);
+<?php if (substr($_GET['file'], -4) === '.php' || strpos($_GET['file'], '../') !== false) { echo 'You are not allowed to see source files!' . "\n"; } else { echo file_get_contents($_GET['file']); } ?>
 else
  echo 'You are not allowed to see source files!'."\n";
 ?>
