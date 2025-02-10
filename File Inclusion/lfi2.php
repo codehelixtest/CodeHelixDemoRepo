2ebcd623814267f7d9c62a95b01004a872fd2c5a
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:22 -0800

    Fix vulnerability in lfi2.php

--- File Inclusion/lfi2.php
+++ File Inclusion/lfi2.php
@@ -1,9 +1,12 @@
 <?php     include("../common/header.php");   ?>
+<!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
+
+<?php hint("will include the arg specified in the GET parameter \"library\", appends .php to end, escape with NULL byte %00"); ?>
+
 <?php include('../common/header.php'); ?>
 <form action="/LFI-2/index.php" method="GET">
     <input type="text" name="library">
 </form>
-
 <?php
 $library = basename($_GET['library']); // Sanitize input
 if (file_exists('includes/' . $library . '.php')) {
@@ -12,10 +15,6 @@ if (file_exists('includes/' . $library . '.php')) {
     // Handle error
 }
 ?>
-
-<?php hint("will include the arg specified in the GET parameter \"library\", appends .php to end, escape with NULL byte %00"); ?>
-
-<form action="/LFI-2/index.php" method="GET">
     <input type="text" name="library">
 </form>
 
