083299b0779b41c27fede0388204984e77b944cb
codehelixtest <rogern@nealit.com>
2025-02-09 18:32:24 -0800

    Fix vulnerability in dom.php

--- XSS/dom.php
+++ XSS/dom.php
@@ -13,28 +13,8 @@ require_once('../_helpers/strip.php');
     <script>
       window.onload = function(){
         let someObject = window.someObject || {};
-<?php
-require_once('../_helpers/strip.php');
-$name = htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8');
-?>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>
-      Hi, <?= $name; ?>
-    </p>
-    <script>
-      window.onload = function(){
-        let someObject = window.someObject || {};
         let script = document.createElement('script');
-        script.src = someObject.url;
-        document.body.appendChild(script);
-     };
-    </script>
-  </body>
-</html>
-        script.src = someObject.url;
+Hi, <?= htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8'); ?>
         document.body.appendChild(script);
      };
     </script>
