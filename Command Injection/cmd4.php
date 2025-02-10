b311259cd09122e54e17e295f5127b46faab54ab
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:18 -0800

    Fix vulnerability in cmd4.php

--- Command Injection/cmd4.php
+++ Command Injection/cmd4.php
@@ -1,4 +1,7 @@
 <?php     include("../common/header.php");   ?>
+<!-- from https://pentesterlab.com/exercises/php_include_and_post_exploitation/course -->
+<?php  hint("will exec 'whois' with the arg specified in the POST parameter \"domain\""); ?>
+
 <?php include("../common/header.php"); ?>
 <form action="/CMD-4/index.php" method="POST">
     <input type="text" name="domain">
@@ -8,11 +11,8 @@
 <?php
     $domain = escapeshellarg($_POST["domain"]);
     system("whois " . $domain);
- ?>
+?>
 </pre>
-<?php  hint("will exec 'whois' with the arg specified in the POST parameter \"domain\""); ?>
-
-<form action="/CMD-4/index.php" method="POST">
     <input type="text" name="domain">
 </form>
 
