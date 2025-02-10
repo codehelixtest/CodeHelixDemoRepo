6cc5af39e9d242226201205266bca22cd937c9fd
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:12 -0800

    Fix vulnerability in koa.js

--- Open Redirect/koa.js
+++ Open Redirect/koa.js
@@ -4,7 +4,7 @@ const urlLib = require('url');
 const app = new Koa();
 
 app.use(async ctx => {
-	var url = ctx.query.target;
+app.use(async ctx => { var url = ctx.query.target; if (!isValidUrl(url)) { ctx.throw(400, 'Invalid URL'); } ctx.redirect(url); }); function isValidUrl(url) { // Implement URL validation logic here }
 	ctx.redirect(url); 
 });
 
