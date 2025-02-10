a2f0092775b6c188dda541ae6bb2b95eda5c88a4
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:59 -0800

    Fix vulnerability in tarlogic1.php

--- PHP Object Injection/tarlogic1.php
+++ PHP Object Injection/tarlogic1.php
@@ -2,30 +2,14 @@
 // Exercise - 0
 // Author: @TheXC3LL
 // Website: ka0labs.net
-$test = new warm();
-$a = serialize($test);
-echo "Example of an object:
-$a
-
-";
-if (isset($argv[1])) {
-    $input = $argv[1];
-    // Validate or sanitize $input here
-    // For example, check if it matches expected patterns
-    if (isValidSerializedData($input)) {
-        unserialize($input);
-    } else {
-        echo "Invalid input data.
-";
-    }
-}
+class warm {
   public $dir = ".";
   public function __wakeup() {
     echo "This folder contains:\n";
     system("ls " . $this->dir);
   }
 }
-$test = new warm();
+$data = isset($argv[1]) ? unserialize($argv[1]) : null; if ($data instanceof warm) { /* safe usage */ } else { /* handle error */ }
 $a = serialize($test);
 echo "Example of an object:\n$a\n\n";
 unserialize($argv[1]);
