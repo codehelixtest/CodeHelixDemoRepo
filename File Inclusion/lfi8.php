c84c1a38816b4598155b2faf56559c1bfe494734
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:11 -0800

    Fix vulnerability in lfi8.php

--- File Inclusion/lfi8.php
+++ File Inclusion/lfi8.php
@@ -1,6 +1,19 @@
 <?php     include("../common/header.php");   ?>
 
-<!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
+<?php include("../common/header.php"); ?>
+
+<form action="/LFI-8/index.php" method="POST">
+    <input type="text" name="file" required>
+</form>
+
+<?php
+if (isset($_POST['file']) && substr($_POST['file'], -4) === '.php') {
+    echo 'You are not allowed to see source files!' . "\n";
+} else {
+    $file = basename($_POST['file']); // Sanitize input
+    echo file_get_contents($file);
+}
+?>
 <?php hint("will include the arg specified in the POST parameter \"file\", looks for .php at end - bypass by apending /. (slash plus dot)"); ?>
 
 <form action="/LFI-8/index.php" method="POST">
@@ -10,20 +23,7 @@
 
 <?php
 if (substr($_POST['file'], -4, 4) != '.php')
-<?php
-if (isset($_POST['file']) && substr($_POST['file'], -4) === '.php') {
-    echo 'You are not allowed to see source files!' . "\n";
-} else {
-    // Sanitize and validate the input to prevent LFI
-    $file = basename($_POST['file']); // Only allow the base name
-    $allowed_files = ['file1.txt', 'file2.txt']; // Example of allowed files
-    if (in_array($file, $allowed_files)) {
-        echo file_get_contents($file);
-    } else {
-        echo 'Invalid file specified.';
-    }
-}
-?>
+ echo file_get_contents($_POST['file']);
 else
  echo 'You are not allowed to see source files!'."\n";
 ?>
