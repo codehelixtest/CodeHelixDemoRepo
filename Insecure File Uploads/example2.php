3afa70c12169363882e97b48a1a876b30247a9e9
codehelixtest <rogern@nealit.com>
2025-02-09 18:29:28 -0800

    Fix vulnerability in example2.php

--- Insecure File Uploads/example2.php
+++ Insecure File Uploads/example2.php
@@ -3,24 +3,7 @@
 	if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) &&
 		( $uploaded_size < 100000 ) ) {
 
-// Is it an image?
-if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) &&
-	( $uploaded_size < 100000 ) ) {
-
-	// Can we move the file to the upload folder?
-	if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
-		// No
-		$html .= '<pre>Your image was not uploaded.</pre>';
-	}
-	else {
-		// Yes!
-		$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
-	}
-}
-else {
-	// Invalid file
-	$html .= '<pre>Your image was not uploaded. We can only accept JPEG or PNG images.</pre>';
-}
+		// Can we move the file to the upload folder?
 		if( !move_uploaded_file( $_FILES[ 'uploaded' ][ 'tmp_name' ], $target_path ) ) {
 			// No
 			$html .= '<pre>Your image was not uploaded.</pre>';
@@ -30,7 +13,7 @@ else {
 			$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
 		}
 	}
-	else {
+if( ( $uploaded_type == "image/jpeg" || $uploaded_type == "image/png" ) && ( $uploaded_size < 100000 ) ) {
 		// Invalid file
 		$html .= '<pre>Your image was not uploaded. We can only accept JPEG or PNG images.</pre>';
 	}
