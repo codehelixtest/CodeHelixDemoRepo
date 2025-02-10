09017a648e6692e90e15ea168f1cd4610eb745a1
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:16 -0800

    Fix vulnerability in dom.php

--- XSS/dom.php
+++ XSS/dom.php
@@ -1,14 +1,33 @@
 <?php
 require_once('../_helpers/strip.php');
 ?>
+<?php
+require_once('../_helpers/strip.php');
+?>
 <html>
+  <head>
+  </head>
+  <body>
+    <p>
+      Hi, <?= htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8'); ?>
+    </p>
+    <script>
+      window.onload = function(){
+        let someObject = window.someObject || {};
+        let script = document.createElement('script');
+        script.src = someObject.url;
+        document.body.appendChild(script);
+     };
+    </script>
+  </body>
+</html>
   <!-- From https://portswigger.net/web-security/dom-based/dom-clobbering -->
   <head>
 
   </head>
   <body>
     <p>
-Hi, <?= htmlspecialchars($_GET['name'], ENT_QUOTES, 'UTF-8'); ?>
+      Hi, <?= $_GET['name']; ?>
     </p>
     <script>
       window.onload = function(){
