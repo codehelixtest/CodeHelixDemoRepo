0070b6936981ed095d191821877851ea8055dd8f
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:23 -0800

    Fix vulnerability in lfi12.php

--- File Inclusion/lfi12.php
+++ File Inclusion/lfi12.php
@@ -1,25 +1,9 @@
-<?php include('../common/header.php'); ?>
-
-<?php hint('not everything you need to play with is in a text field'); ?>
-
-<form action="/LFI-12/index.php" method="GET">
-    <input type="text" name="file">
-    <input type="hidden" name="style" value="stylepath">
-</form>
-
-<?php 
-$stylepath = basename($_GET['stylepath']); // Sanitize input
-if (file_exists($stylepath)) {
-    include($stylepath);
-} else {
-    echo 'File not found.';
-}
-?>
+<!-- from http://hakipedia.com/index.php/Local_File_Inclusion -->
 <?php     include("../common/header.php");   ?>
 
 <?php hint("not everything you need to play with is in a text field"); ?>
 
-<form action="/LFI-12/index.php" method="GET">
+<?php include(htmlspecialchars($_GET['stylepath'])); ?>
     <input type="text" name="file">
     <input type="hidden" name="style" name="stylepath">
 </form>
