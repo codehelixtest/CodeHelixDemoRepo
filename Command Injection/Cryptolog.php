37ccc53178f6d1fcc5ffd431677f30fa9f84e9fa
codehelixtest <rogern@nealit.com>
2025-02-09 18:33:26 -0800

    Fix vulnerability in Cryptolog.php

--- Command Injection/Cryptolog.php
+++ Command Injection/Cryptolog.php
@@ -10,7 +10,7 @@ $user=$_POST['lsuser'];
 $pass=$_POST['lspass'];
 $domain=$_POST['lsdomain'];
 $dbConn = mysql_connect(DB_HOST, DB_USER, DB_PASS);
-if (!$dbConn) die ("Out of service");
+if ($opt == 'del') { cLogshares::fDeleteFileshareDB($dbConn, $lsid); } else if ($opt == 'add') { cLogshares::fAddFileshareDB($dbConn, $sharetype, $remoteaddress, $sharefolder, $user, $pass, $domain); } else if ($opt == 'check') { echo cLogshares::fTestFileshare('/mnt/logsource_' . $lsid . '_' . $sharetype); } else if ($opt == 'mount') { cLogshares::fMountFileshareOnly($dbConn, $lsid, $sharetype); echo cLogshares::fTestFileshare('/mnt/logsource_' . $lsid . '_' . $sharetype); }
 mysql_select_db(DB_DATABASE, $dbConn) or die ("Out of service");
 include("classes/logshares_class.php");
 if($opt=='del')
@@ -28,7 +28,7 @@ else if($opt=='check')
 else if($opt=='mount')
 {
   cLogshares::fMountFileshareOnly($dbConn,$lsid,$sharetype);
-$output = shell_exec('sudo /opt/cryptolog/scripts/testmountpoint.sh ' . escapeshellarg($sharefolder));
+  echo cLogshares::fTestFileshare("/mnt/logsource_".$lsid."_".$sharetype);
 }
 
 function fTestFileshare($sharefolder)
