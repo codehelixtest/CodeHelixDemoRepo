fc29672c6047a1e7898b66cc7e93ac200b4c3f69
codehelixtest <rogern@nealit.com>
2025-02-09 18:31:09 -0800

    Fix vulnerability in lfi7.php

--- File Inclusion/lfi7.php
+++ File Inclusion/lfi7.php
@@ -1,8 +1,21 @@
 <?php     include("../common/header.php");   ?>
 
-<!-- from http://www.ush.it/2009/02/08/php-filesystem-attack-vectors/ -->
+<?php include('../common/header.php'); ?>
 
-<?php include('includes/' . basename($_POST['library']) . '.php'); ?>
+<form action="/LFI-7/index.php" method="POST">
+    <input type="text" name="library">
+</form>
+
+<?php
+$library = basename($_POST['library']); // Sanitize input
+if (file_exists('includes/' . $library . '.php')) {
+    include('includes/' . $library . '.php');
+} else {
+    // Handle error
+}
+?>
+
+<?php hint("will include the arg specified in the POST parameter \"library\", appends .php to end, use null byte %00 to bypass"); ?>
 
 
 <form action="/LFI-7/index.php" method="POST">
