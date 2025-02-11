47ebb745ae4ce95de4a49461d5dc6abdcb12d391
codehelixtest <rogern@nealit.com>
2025-02-11 12:02:04 -0800

    Fix vulnerability in Cryptolog.php

--- Command Injection/Cryptolog.php
+++ Command Injection/Cryptolog.php
@@ -1,5 +1,5 @@
 <?php
-include("config.php");
+require_once('config.php');
 require_once("kontrol.php");
 $opt=$_POST['opt'];
 $lsid=$_POST['lsid'];
@@ -17,7 +17,7 @@ if($opt=='del')
 {
   cLogshares::fDeleteFileshareDB($dbConn,$lsid);
 }
-else if($opt=='add')
+if ($opt == 'del') { cLogshares::fDeleteFileshareDB($dbConn, $lsid); } else if ($opt == 'add') { cLogshares::fAddFileshareDB($dbConn, $sharetype, $remoteaddress, $sharefolder, $user, $pass, $domain); } else if ($opt == 'check') { echo cLogshares::fTestFileshare('/mnt/logsource_' . $lsid . '_' . $sharetype); } else if ($opt == 'mount') { cLogshares::fMountFileshareOnly($dbConn, $lsid, $sharetype); echo cLogshares::fTestFileshare('/mnt/logsource_' . $lsid . '_' . $sharetype); }
 {
   cLogshares::fAddFileshareDB($dbConn,$sharetype,$remoteaddress,$sharefolder,$user,$pass,$domain);
 }
